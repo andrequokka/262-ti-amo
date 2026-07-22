2e75685d2e68bd4d0340f6ffc4b2cafbd2be0daf
andrequokka <limonegiallo305@gmail.com>
2026-07-22 21:50:08 +0200

    Remove player movement event listeners and variables
    Removed player movement logic and related variables.

--- script.js
+++ script.js
@@ -45,33 +45,7 @@ lettera.addEventListener("scroll", () => {
     scrollHint.classList.add("hidden");
 });
 };
-const player = document.getElementById("player");
 
-let x = 40;
-let y = 180;
-
-document.addEventListener("keydown", (e)=>{
-
-    if(e.key=="ArrowLeft"){
-        x-=4;
-    }
-
-    if(e.key=="ArrowRight"){
-        x+=4;
-    }
-
-    if(e.key=="ArrowUp"){
-        y-=4;
-    }
-
-    if(e.key=="ArrowDown"){
-        y+=4;
-    }
-
-    player.style.left=x+"px";
-    player.style.top=y+"px";
-
-});
 // ====================
 // MOVIMENTO FEDE
 // ====================
